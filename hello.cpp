b9bd65b10810e6fa234aef32fbe11e3e29580a07
Shriraam-Thiagarajan <163787070+Shriraam-Thiagarajan@users.noreply.github.com>
2026-01-28 15:07:11 +0000

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, git" << std::endl;
+    std::cout << "Hello, git pull test" << std::endl;
     std::cout << "I am testing branches with this" << std::endl;
     return 0;
 }

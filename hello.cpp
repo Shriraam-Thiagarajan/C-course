4184ec12b1318b0ea4ea5d4a12c67081b1db1dd0
Shriraam Thiagarajan <zcapsth@ucl.ac.uk>
2026-01-28 15:03:24 +0000

    change hello

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, git" << std::endl;
+    std::cout << "Hello" << std::endl;
     std::cout << "I am testing branches with this" << std::endl;
     return 0;
 }
